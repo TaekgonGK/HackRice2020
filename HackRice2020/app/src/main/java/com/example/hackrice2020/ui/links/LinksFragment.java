aed30d0e7c430103256b5bd8607b8fd51589d897
jihovictoria <jihovictoria@gmail.com>
2020-09-20 05:13:21 -0700

    okie2

--- HackRice2020/app/src/main/java/com/example/hackrice2020/ui/links/LinksFragment.java
+++ HackRice2020/app/src/main/java/com/example/hackrice2020/ui/links/LinksFragment.java
@@ -1,11 +1,16 @@
 package com.example.hackrice2020.ui.links;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -30,18 +35,47 @@ public class LinksFragment extends Fragment{
             }
         });
 
-        TextView text = (TextView) root.findViewById(R.id.text_links);
+        ImageButton cdc = (ImageButton) root.findViewById(R.id.cdcButton);
+        cdc.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+            {
+                goToUrl("https://www.cdc.gov/coronavirus/2019-ncov/index.html");
+            }
+        });
+
+        ImageButton who = (ImageButton) root.findViewById(R.id.whoButton);
+        who.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+            {
+                goToUrl("https://www.who.int/emergencies/diseases/novel-coronavirus-2019");
+            }
+        });
+
+        ImageButton jh = (ImageButton) root.findViewById(R.id.jhButton);
+        jh.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+            {
+                goToUrl("https://coronavirus.jhu.edu/map.html");
+            }
+        });
+
+        /**TextView text = (TextView) root.findViewById(R.id.text_links);
         text.setMovementMethod(LinkMovementMethod.getInstance());
 
         text = (TextView) root.findViewById(R.id.text_links1);
         text.setMovementMethod(LinkMovementMethod.getInstance());
 
         text = (TextView) root.findViewById(R.id.text_links2);
-        text.setMovementMethod(LinkMovementMethod.getInstance());
+        text.setMovementMethod(LinkMovementMethod.getInstance());**/
 
 
 
         return root;
     }
 
+    private void goToUrl (String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchBrowser);
+    }
 }
